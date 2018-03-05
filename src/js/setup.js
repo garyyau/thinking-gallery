9fbb14e3bf237fb562c81a9ee8f98bb14aea3406
Gary Yau <gyau88@gmail.com>
2018-03-05 10:24:26 -0800

    Added full modal functionality.

--- src/js/setup.js
+++ src/js/setup.js
@@ -3,8 +3,12 @@ import data from '../../data.json'
 const updateImageModal = (image) => {
   const container = document.getElementById("modal-container");
   const modal = document.getElementById("modal");
-  const description = modal.getElementsByClassName("modal__description")[0];
 
+  const imgContainer = modal.getElementsByClassName("modal__image")[0];
+  const imgElm = imgContainer.getElementsByTagName("img")[0];
+  imgElm.src = image.url;
+
+  const description = modal.getElementsByClassName("modal__description")[0];
   description.innerHTML = image.description;
 
   const classes = container.className.split(" ");
@@ -30,6 +34,19 @@ const createImageContainers = () => {
   });
 };
 
+const setupImageModalClose = () => {
+  const container = document.getElementById("modal-container");
+  const modal = document.getElementById("modal");
+  const close = modal.getElementsByClassName("modal__close")[0];
+
+  close.onclick = () => {
+    const classes = container.className.split(" ");
+    classes.push("hide");
+    container.className = classes.join(" ");
+  }
+};
+
 export const setup = () => {
   createImageContainers();
+  setupImageModalClose();
 };
