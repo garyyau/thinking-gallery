9f407bd7ada78751b03e4739b95e075f6c4d1f7a
Gary Yau <gyau88@gmail.com>
2018-03-06 00:05:31 -0800

    Added fix on navbar for responsive animation.

--- src/js/navbar.js
+++ src/js/navbar.js
@@ -1,35 +1,66 @@
 const navbar = document.getElementById("navbar");
+const breakpoint = 768;
 
 const animateIn = () => {
   const timeline = new TimelineLite();
-  const width = `${navbar.offsetWidth}`;
 
-  timeline.set(navbar, { css: { marginLeft: `-${width}px` }}, 0);
+  const attrStart = {};
+  const attrEnd = {};
+  if (window.innerWidth < breakpoint) {
+    attrStart.marginLeft = 'auto';
+    attrStart.marginTop = `-${navbar.offsetHeight}px`;
+    attrEnd.marginLeft = 'auto';
+    attrEnd.marginTop = 0;
+  } else {
+    attrStart.marginLeft = `-${navbar.offsetWidth}px`;
+    attrStart.marginTop = 'auto';
+    attrEnd.marginLeft = 0;
+    attrEnd.marginTop = 'auto';
+  }
+
+  timeline.set(navbar, { css: attrStart }, 0);
   timeline.fromTo(navbar, 0.3, {
-    css: { marginLeft: `-${width}px` }
+    css: attrStart
   }, {
-    css: { marginLeft: 0 },
+    css: attrEnd,
     ease: Power3.easeOut
   }, 0);
-  timeline.set(navbar, { css: { marginLeft: 0 }});
+  timeline.set(navbar, { css: attrEnd });
 };
 
 const animateOut = () => {
   const timeline = new TimelineLite();
-  const width = `${navbar.offsetWidth}`;
 
-  timeline.set(navbar, { css: { marginLeft: 0 }}, 0);
+  const attrStart = {};
+  const attrEnd = {};
+  if (window.innerWidth < breakpoint) {
+    attrStart.marginLeft = 'auto';
+    attrStart.marginTop = 0;
+    attrEnd.marginLeft = 'auto';
+    attrEnd.marginTop = `-${navbar.offsetHeight}px`;
+  } else {
+    attrStart.marginLeft = 0;
+    attrStart.marginTop = 'auto';
+    attrEnd.marginLeft = `-${navbar.offsetWidth}px`;
+    attrEnd.marginTop = 'auto';
+  }
+
+  timeline.set(navbar, { css: attrStart }, 0);
   timeline.fromTo(navbar, 0.3, {
-    css: { marginLeft: 0 }
+    css: attrStart
   }, {
-    css: { marginLeft: `-${width}px` },
+    css: attrEnd,
     ease: Power3.easeIn
   }, 0);
-  timeline.set(navbar, { css: { marginLeft: `-${width}px` }});
+  timeline.set(navbar, { css: attrEnd });
 };
 
 const isVisible = () => {
-  return navbar.offsetLeft > 0;
+  if (window.innerWidth < breakpoint) {
+    return navbar.offsetTop > 0;
+  } else {
+    return navbar.offsetLeft > 0;
+  }
 }
 
 export const hideNavbar = () => {
